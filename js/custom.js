d3553ef74708ce57495b4343c6e649004b2ae5df
mukesh569 <mukeshsharma569@gmail.com>
2021-06-02 19:15:46 +0530

    Revert "ui fixed"
    This reverts commit 06b70a17eb20a7850e24d4836e583a12cbc9538d.

--- js/custom.js
+++ js/custom.js
@@ -1,34 +1,25 @@
- var isPlay = false;
- const play = document.getElementsByClassName("play");
- for (var i = 0; i < play.length; i++) { 
-  play[i].addEventListener("click", function(i){
-   // console.log(this.parentElement.parentElement.children[3]);
-   let  playMusic= ()=> {
-    let music = this.parentElement.parentElement.children[3];
-     const img = this.parentElement.parentElement.children[2].children[0];
+const play = document.getElementById("play");
+ const music = document.querySelector("audio");
+  const img = document.getElementById("img");
+
+ let isPlay = false;
+ //play functiom
+ const playMusic= ()=>{
+    isPlay = true;
      music.play();
-     this.classList.replace("fa-play","fa-pause");
+     play.classList.replace("fa-play","fa-pause");
      img.classList.add("anime");
-     isPlay = true;
  };
- let pauseMusic= ()=>{
-  
-   
-    let music = this.parentElement.parentElement.children[3];
-    const img = this.parentElement.parentElement.children[2].children[0];
+ //pause function
+ const pauseMusic= ()=>{
+    isPlay = false;
      music.pause();
-     this.classList.replace("fa-pause","fa-play");
+     play.classList.replace("fa-pause","fa-play");
      img.classList.remove("anime");
-       isPlay = false;
-};  
-     
-      isPlay ? pauseMusic() : playMusic();
-      
- 
-
-  });
-}
-
+ };
+ play.addEventListener("click",()=>{
+     isPlay ? pauseMusic() : playMusic();
+ });
 
 
 
@@ -51,13 +42,10 @@ $(function() {
       $(".scroll-up").css("display","none");
     }
   });
-
  $(".mobile-menu").click(function(){
    $(".menu").toggle("slow");
  })
   });
-
-
   filterSelection("all")
 function filterSelection(c) {
   var x, i;
@@ -93,7 +81,7 @@ function w3RemoveClass(element, name) {
 
 // Add active class to the current button (highlight it)
 var btnContainer = document.getElementById("myBtnContainer");
-var btns = document.getElementsByClassName("btn-photo");
+var btns = btnContainer.getElementsByClassName("btn-photo");
 for (var i = 0; i < btns.length; i++) {
   btns[i].addEventListener("click", function(){
     var current = document.getElementsByClassName("myactive");
@@ -107,6 +95,7 @@ function myFunction(imgs) {
   expandImg.src = imgs.childNodes[1].childNodes[1].src; 
   expandImg.parentElement.style.display = "block";
 }
+
 
 
 
